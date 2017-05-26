2f83aa9c00d7e7a16c6ffeed5724c7df4401aeb4
Giovane Cunha Mocellin <giovane.mocellin@gmail.com>
2017-05-25 23:39:21 -0300

    Implemented: dc_v, variaveis, mais_var, dc_p, parametros, ... , pfalsa

--- SyntacticAnalysis.cpp
+++ SyntacticAnalysis.cpp
@@ -79,55 +79,162 @@ void dc_c(std::vector<string>& S) {
 }
 
 void dc_v(std::vector<string>& S) {
-    
+    string token = lexicalAnalysis();
+    if (token == "var") {
+        variaveis(S);
+        token = lexicalAnalysis();
+        if (token != "colon") {
+            printError(":");
+            token = panic(S);
+        }
+        tipo_var(S);
+        token = lexicalAnalysis();
+        if (token != "tipe_real" &&  token != "tipo_integer") {
+            printError("tipe real or integer");
+            token = panic(S);
+        }
+        token = lexicalAnalysis();
+        if (token != "semicolon") {
+            printError(";");
+            token = panic(S);
+        }
+        dc_v(S);
+    } else {
+        goBackOneToken();
+    }
 }
 
 void variaveis(std::vector<string>& S) {
-    
+    token = lexicalAnalysis();
+    if (token != "ident") {
+        printError("ident");
+        token = panic(S);
+    }
+    mais_var(S);
 }
 
 void mais_var(std::vector<string>& S) {
-    
+    token = lexicalAnalysis();
+    if (token != "comma") {
+        printError(",");
+        token = panic(S);
+    }
+    variaveis(S);
 }
 
 void dc_p(std::vector<string>& S) {
-    
+    string token = lexicalAnalysis();
+    if (token == "procedure") {
+        token = lexicalAnalysis();
+        if (token != "ident") {
+            printError("ident");
+            token = panic(S);
+        }
+        parametros(S);
+        token = lexicalAnalysis();
+        if (token != "semicolon") {
+            printError(";");
+            token = panic(S);
+        }
+        corpo_p(S);
+        dc_p(S);
+    } else {
+        goBackOneToken();
+    }
 }
 
 void parametros(std::vector<string>& S) {
-    
+    token = lexicalAnalysis();
+    if (token != "paren_left") {
+        printError("(");
+        token = panic(S);
+    }
+    lista_par(S);
+    token = lexicalAnalysis();
+    if (token != "paren_right") {
+        printError(")");
+        token = panic(S);
+    }
 }
 
 void lista_par(std::vector<string>& S) {
-    
+    variaveis(S);
+    token = lexicalAnalysis();
+    if (token != "colon") {
+        printError(":");
+        token = panic(S);
+    }
+    tipo_var(S);
+    mais_var(S);
 }
 
 void mais_par(std::vector<string>& S) {
-    
+    token = lexicalAnalysis();
+    if (token != "semicolon") {
+        printError(";");
+        token = panic(S);
+    }
+    lista_par(S);
 }
 
 void corpo_p(std::vector<string>& S) {
-    
+    dc_loc(S);
+    token = lexicalAnalysis();
+    if (token != "begin") {
+        printError("begin");
+        token = panic(S);
+    }
+    comandos(S);
+    token = lexicalAnalysis();
+    if (token != "end") {
+        printError("end");
+        token = panic(S);
+    }
 }
 
 void dc_loc(std::vector<string>& S) {
-    
+    dc_v(S);
 }
 
 void lista_arg(std::vector<string>& S) {
-    
+    token = lexicalAnalysis();
+    if (token != "paren_left") {
+        printError("(");
+        token = panic(S);
+    }
+    argumentos(S);
+    token = lexicalAnalysis();
+    if (token != "paren_right") {
+        printError(")");
+        token = panic(S);
+    }
 }
 
 void argumentos(std::vector<string>& S) {
-    
+    token = lexicalAnalysis();
+    if (token != "ident") {
+        printError("ident");
+        token = panic(S);
+    }
+    cmd(S);
 }
 
 void mais_ident(std::vector<string>& S) {
-    
+    token = lexicalAnalysis();
+    if (token != "semicolon") {
+        printError(";");
+        token = panic(S);
+    }
+    argumentos(S);
 }
 
 void pfalsa(std::vector<string>& S) {
-    
+    token = lexicalAnalysis();
+    if (token != "else") {
+        printError("else");
+        token = panic(S);
+    }
+    cmd(S);
 }
 
 void comandos(std::vector<string>& S) {
@@ -161,11 +268,6 @@ void mais_fatores(std::vector<string>& S) {
 void fator(std::vector<string>& S) {
     string token = lexicalAnalysis();
     if (token == "ident")  { return;} 
-<<<<<<< HEAD
-
-    else if (token == "real" || token == "integer") { return;}
-=======
->>>>>>> bbdfe483c7d64f25877e44bf98956d311995b907
 
     else if (token == "paren_left") {
         expressao();
@@ -178,7 +280,6 @@ void fator(std::vector<string>& S) {
 
     else if (numero()) { return;}
     
-
     else{
         printError("ident or numero or (");
         token = panic(S);
