bc594b0ce56d086dd64152469af5951341348edd
Lais Helena Chiachio de Miranda <lalena.chiachio@gmail.com>
2017-05-25 22:57:07 -0300

    implement fator

--- SyntacticAnalysis.cpp
+++ SyntacticAnalysis.cpp
@@ -129,5 +129,24 @@ void mais_fatores(std::vector<string>& S) {
 }
 
 void fator(std::vector<string>& S) {
-    
+    string token = lexicalAnalysis();
+    if (token == "ident"  { return;} 
+
+    else if (token == "real" || token == "integer") { return;}
+
+    else if (token == "paren_left") {
+        expressao();
+        token = lexicalAnalysis();
+        if(token != "paren_right"){
+            printError(")");
+            token = panic(S);
+        }
+    }
+
+    else{
+        printError("ident or numero or (");
+        token = panic(S);
+
+    }
 }
+    
